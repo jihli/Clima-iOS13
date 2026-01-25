4ce0f3d06a923db80d848081c16d623f125d4d20
jihaoli <a276901052@outlook.com>
2026-01-25 21:43:59 +0800

    Write Detailed Comment for current code

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,32 +8,41 @@
 
 import UIKit
 
+//UIViewController：继承苹果的页面基类，才能拥有生命周期（viewDidLoad 等）和 UI 管理能力
+//UITextFieldDelegate：表示这个类要实现 文本框代理。代理用于接收文本框事件（比如按 Return、结束编辑、是否允许结束编辑等）
 class WeatherViewController: UIViewController,UITextFieldDelegate {
-
+    // @IBOutlet：告诉 Xcode 这个属性可以在 Interface Builder（storyboard）里连线。不同的元素已经和这里的代码建立联系了
+    //weak：弱引用，避免循环引用导致内存泄漏
+    //var xxx: 类型!：UIImageView / UILabel / UITextField：控件类型。
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
     
+    //super.viewDidLoad()：先让父类做它必须做的初始化
+    //searchTextField.delegate = self: 把 searchTextField 的代理设置为当前控制器,这样下面这些代理方法才会被触发：textFieldShouldReturn + textFieldShouldEndEditing + textFieldDidEndEditing
     override func viewDidLoad() {
         super.viewDidLoad()
         searchTextField.delegate = self
-        
-    }
-
-
-    @IBAction func searchPressed(_ sender: UIButton) {
-        searchTextField.endEditing(true)
-        print(searchTextField.text!)
-        
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        searchTextField.endEditing(true)
+    // _ sender这里的"_"表示在调用函数的时候 不用强制写出Name = xxx, 直接输入xxx就可以
+    //searchTextField.endEditing(true) -> 会接下来调用textFieldShouldEndEditing + textFieldDidEndEditing
+    @IBAction func searchPressed(_ sender: UIButton) {
         print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+    
+    // textFieldShouldReturn处理的是user在当前唯一的一个UITextField界面中
+    // 输入return以后 做的事情 这里会首先print(searchTextField.text!)
+    // 然后调用textFieldShouldEndEditing + textFieldDidEndEditing 把搜索框的字变成""
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
         return true
     }
     
+    // textFieldShouldEndEditing会对应不应该把搜索框字清零进行判断
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         if textField.text != "" {
             return true
@@ -43,6 +52,7 @@ class WeatherViewController: UIViewController,UITextFieldDelegate {
         }
     }
     
+    // textFieldDidEndEditing对搜索框的内容 变成""
     func textFieldDidEndEditing(_ textField: UITextField) {
         // Use searchTextField.text to get the weather for that city
         searchTextField.text = ""
