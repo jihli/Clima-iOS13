8fd3a8fd7f591c589ac2e437e277e253113cdfcd
jihaoli <a276901052@outlook.com>
2026-02-19 22:08:12 +0800

    create parseJSON() and create weatherData object for holding json content

---
+++ Clima/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by JIHAO LI on 2/19/26.
+//  Copyright © 2026 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name:String
+    let main:Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp:Double
+}
+
+
+struct Weather:Decodable {
+    let description:String
+}
--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -12,7 +12,7 @@ struct WeatherManager {
     
     func fetchWeather(cityName:String){
         let urlString = "\(weatherURL)&q=\(cityName)"
-        performRequest(urlString: urlString)
+        self.performRequest(urlString: urlString)
     }
     
     func performRequest(urlString:String){
@@ -20,9 +20,22 @@ struct WeatherManager {
         if let url = URL(string:urlString){
             // 2. Create a URLSession
             let session = URLSession(configuration:.default)
-    
-            // 3. Give Session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:  response:  error: ))
+            
+            // 3. Give Session a task (如下应用了closure function也就是类似python的lambda function)
+            let task = session.dataTask(with: url) {(data,response,error) in
+                // data 是具体的返回值 jason返回天气的具体参数和内容
+                // response包含status和header -> status就是200 / 404之类的
+                // error是只有在返回失败的时候 才会出现
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data{
+                    self.parseJSON(weatherData:safeData)
+                }
+            }
+            
             //创建一个任务去拉取 URL内容，请求结束后调用你提供的 completionHandler 回调，把 Data/Response/Error 传给你。
             //所以这里的input其实是系统在得到url的内容以后 解析成为了data/response/error 返回给你的 你接受了以后在handle中处理逻辑
             
@@ -31,18 +44,14 @@ struct WeatherManager {
         }
     }
     
-    func handle(data:Data?, response:URLResponse?,error:Error?){
-        // data 是具体的返回值 jason返回天气的具体参数和内容
-        // response包含status和header -> status就是200 / 404之类的
-        // error是只有在返回失败的时候 才会出现
-        if error != nil {
-            print(error!)
-            return
+    func parseJSON(weatherData: Data){
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
         
-        if let safeData = data{
-            let dataString = String(data:safeData, encoding:.utf8)
-            print(dataString)
-        }
     }
 }
